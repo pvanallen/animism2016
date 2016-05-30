e002cda23d25dbf0516d8daf4f2b8524e636f9d2
phil <pva@philvanallen.com>
2016-05-30 14:16:38 -0700

    initial files

---
+++ send_notifications.py
@@ -0,0 +1,18 @@
+import random
+
+def get_provocations(file_name):
+	try:
+		fo = open(file_name, "r")
+		provocations = fo.read().split('\n')
+
+	except:
+		logging.info('no sync file found')
+		provocations = ['None found']
+
+	else:
+		fo.close()
+
+	return provocations
+
+provocation = random.choice(get_provocations('oblique_strategies.txt'))
+print(provocation)
